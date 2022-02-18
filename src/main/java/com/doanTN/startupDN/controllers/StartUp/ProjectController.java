b46d15964d681eb62d6b16431a440ea858b419f1
Vo Ngoc Trai <vongoctrai>
2022-02-19 00:38:35 +0700

    Trai commit

--- src/main/java/com/doanTN/startupDN/controllers/StartUp/ProjectController.java
+++ src/main/java/com/doanTN/startupDN/controllers/StartUp/ProjectController.java
@@ -123,12 +123,7 @@ public class ProjectController {
         } else {
             if (projectForm.getId() == null || projectForm.getId().equals("")) {
                 java.util.Date posteddate = new java.util.Date();
-                String imgPresent = "";
-                if(imagepresent.getOriginalFilename() == "" || imagepresent.getOriginalFilename() == null){
-                    imgPresent = "noImage.png";
-                }else {
-                    imgPresent = imagepresent.getOriginalFilename();
-                }
+                String imgPresent = imagepresent.getOriginalFilename();
                 Path imgPresentPath = Paths.get("src/main/resources/static/images/projects/" + imgPresent);
                 Files.write(imgPresentPath, imagepresent.getBytes());
                 Projects project = projectService.saveProject(userDAO.getUsersByUsername(user.getUsername()), categoryService.getCategoryById(projectForm.getCategoryId()),
@@ -177,12 +172,7 @@ public class ProjectController {
                         }
                     });
                 } else {
-                    String imgPresent = "";
-                    if(imagepresent.getOriginalFilename() == "" || imagepresent.getOriginalFilename() == null){
-                        imgPresent = "noImage.png";
-                    }else {
-                        imgPresent = imagepresent.getOriginalFilename();
-                    }
+                    String imgPresent = imagepresent.getOriginalFilename();
                     Path imgPresentPath = Paths.get("src/main/resources/static/images/projects/" + imgPresent);
                     Files.write(imgPresentPath, imagepresent.getBytes());
                     Projects project = projectService.updateProject(projectForm.getId(), categoryService.getCategoryById(projectForm.getCategoryId()),
